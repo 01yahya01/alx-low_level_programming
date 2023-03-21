3ac274918ba9e421e97382b6b53b384aa4ba1b16
01yahya01 <moulayyahya95@gmail.com>
2023-03-21 07:28:51 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,25 +2,25 @@
 
 /**
  * print_sign - Prints the dign of a number
- * @c: The number to be checked
- * Return: 1 for positive num, -1 for negative num or zero for anything else
+ * @n: number to check
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(43);
+	_putchar('+');
 	return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-	_putchar(45);
-	return (-1);
+	_putchar('0');
+	return (0);
 	}
 	else
 	{
-	_putchar(48);
-	return (0);
+	_putchar('-');
+	return (-1);
 	}
 }
