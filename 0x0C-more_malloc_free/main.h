d68d44b22fbd95a80cce0e3a89b4d3f5f410a613
01yahya01 <moulayyahya95@gmail.com>
2023-04-13 08:28:11 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
 
@@ -10,5 +15,11 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+void errors(void);
+
+int _strlen(char *s);
+
+int is_digit(char *s);
 
 #endif
